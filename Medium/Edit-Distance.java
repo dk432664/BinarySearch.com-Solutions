993d6ecca10d462e4d386f99936fc0f77799503e
DeepKothari <43929505+dk432664@users.noreply.github.com>
2020-11-29 09:28:55 +0530

    Edit Distance

---
+++ Medium/Edit-Distance.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solve(String a, String b) {
+        int dp[][]=new int[a.length()+1][b.length()+1];
+       for(int i=0; i<=a.length(); i++)
+        {
+            for(int j=0; j<=b.length(); j++)
+            {
+                if(i==0)
+                dp[i][j]=j;
+               else if(j==0)
+                dp[i][j]=i;
+                else if(a.charAt(i-1)==b.charAt(j-1))
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=1+ Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[a.length()][b.length()];
+    }
+}
