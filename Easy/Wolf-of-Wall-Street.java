d705853e1f393dc40686df4dbe3124ed741e1a84
DeepKothari <43929505+dk432664@users.noreply.github.com>
2020-11-29 11:01:19 +0530

    Wolf of Wall Street

---
+++ Easy/Wolf-of-Wall-Street.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solve(int[] prices) {
+        int profit=0;
+        int min=Integer.MAX_VALUE;
+        for(int i=0; i<prices.length; i++)
+        {
+            if(prices[i]<min)
+            min=prices[i];
+            else if(prices[i]-min>=profit)
+            profit=prices[i]-min;
+        }
+        return profit;
+    }
+}
